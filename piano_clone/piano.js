b9cfc824dbcda9b053ca30751d9b2c9012e97a3c
JimmyHoward <ongkhowe@gmail.com>
2022-02-27 09:00:09 +0800

    fixed piano bug

--- piano_clone/piano.js
+++ piano_clone/piano.js
@@ -2,7 +2,9 @@ const keys = document.querySelectorAll('.key')
 const WHITE_KEYS = ['z','x','c','v','b','n','m']
 const BLACK_KEYS = ['s','d','g','h','j']
 
-keys.forEach(key => addEventListener('click', () => playNote(key)))
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+keys.forEach(key => key.addEventListener('click', () => playNote(key)))
 
 document.addEventListener('keydown', e => {
     if (e.repeat) return
@@ -10,14 +12,15 @@ document.addEventListener('keydown', e => {
     const whiteKeyIndex = WHITE_KEYS.indexOf(key)
     const blackKeyIndex = BLACK_KEYS.indexOf(key)
 
-    if (whiteKeyIndex > -1) playNote(WHITE_KEYS[whiteKeyIndex])
-    if (blackKeyIndex > -1) playNote(BLACK_KEYS[blackKeyIndex])
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
 })
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0
-    note.play()
+    console.log(noteAudio)
+    noteAudio.play()
     key.classList.add('active')
     noteAudio.addEventListener('ended', ()=> {
         key.classList.remove('active')
